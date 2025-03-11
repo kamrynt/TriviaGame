d9192552b0fb4c8b7a3b2b7f1966adee88a69eb2
kamrynt <kamryn.tate@bison.howard.edu>
2025-03-11 19:21:52 -0400

    Initial Commit

---
+++ Trivia/TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaApp.swift
+//  Trivia
+//
+//  Created by Kamryn Tate on 3/11/25.
+//
+
+import SwiftUI
+
+@main
+struct TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
